0340559c9ad8ee5bafe0bf597b8c080000da40d8
pushpatumma <60314552+pushpatumma@users.noreply.github.com>
2020-03-07 21:27:40 +0530

    Add files via upload

---
+++ Main6.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class Main6 extends List6{
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		List6 l=new Lists();
+		System.out.println("Enter Your Choice \n1.Insert\n2.Delete\n3.Search\n4.view\n5.Exit");
+		int ch=sc.nextInt();
+		switch(ch)
+		{
+		case 1:System.out.println("Enter element");
+				int ele=sc.nextInt();
+				System.out.println("Enter position");
+				int pos=sc.nextInt();
+				l.insert(pos, ele);
+				break;
+		case 2:
+			System.out.println("Enter element");
+			int ele1=sc.nextInt();
+			l.remove(ele1);
+			break;
+		case 3:
+			System.out.println("Enter element");
+			int ele2=sc.nextInt();
+			boolean b=l.search(ele2);
+			if(b)
+			{
+				System.out.println("Element Found");
+			}
+			else
+				System.out.println("Element not found");
+			break;
+		case 4:
+			l.printelements();
+		case 5:
+			return;
+		}
+	}
+
+}
